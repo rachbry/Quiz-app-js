8f1c6935fab11aa540d134d351aa47501c6ca8a5
Rachel Cutler <rach_cutler@live.co.uk>
2023-10-23 11:11:01 +0000

    Added questions to js

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+const questions = [
+    {
+        question: "What is the largest animal in the world?",
+        answers: [
+            {text: "Shark", correct: false},
+            {text: "Blue whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
+        ]
+    },
+    {
+        question: "What is smallest country in the world?",
+        answers: [
+            {text: "Vatican City", correct: true},
+            {text: "Bhutan", correct: false},
+            {text: "Nepal", correct: false},
+            {text: "Shri Lanka", correct: false},
+        ]
+    },
+    {
+        question: "Which is the largest desert in the world?",
+        answers: [
+            {text: "Kalahari", correct: false},
+            {text: "Gobi", correct: false},
+            {text: "Sahara", correct: false},
+            {text: "Antartica", correct: true},
+        ]
+    },
+    {
+        question: "Which is the smallest continent in the world?",
+        answers: [
+            {text: "Asia", correct: false},
+            {text: "Australia", correct: true},
+            {text: "Artic", correct: false},
+            {text: "Africa", correct: false},
+        ]
+    }
+]
